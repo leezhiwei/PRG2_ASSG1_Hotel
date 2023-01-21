58ec9e8ba99727b3728a6aa9019df21c28aafa40
leezhiwei <zhiweilee.com@gmail.com>
2023-01-21 21:06:31 +0800

    Imported CSV into objects.

--- PRG2_ASSG1_Hotel/Program.cs
+++ PRG2_ASSG1_Hotel/Program.cs
@@ -3,15 +3,14 @@
 using PRG2_ASSG1_Hotel;
 
 List<Guest> guestList = new List<Guest>();
-
 void InitData()
 {
     List<List<string>> roomlist = new List<List<string>>();
-    List<List<string>> guestlist = new List<List<string>>();
+    List<List<string>> guestlist = new List<List<string>>(); // init string-list var
     List<List<string>> stayslist = new List<List<string>>();
     //Reading data from csv
     string[] roomdata = File.ReadAllLines("Rooms.csv");
-    string[] guestdata = File.ReadAllLines("Guests.csv");
+    string[] guestdata = File.ReadAllLines("Guests.csv"); // readalllines of CSV
     string[] staysdata = File.ReadAllLines("Stays.csv");
     for (int i = 1; i < roomdata.Length; i++)
     {
@@ -23,7 +22,7 @@ void InitData()
     {
         List<string> values = guestdata[i].Split(',').ToList();
         //Adding data into list
-        guestlist.Add(values);
+        guestlist.Add(values); 
     }
     for (int i = 1; i < staysdata.Length; i++)
     {
@@ -31,11 +30,39 @@ void InitData()
         //Adding data into list
         stayslist.Add(values);
     }
-    foreach (List<string> guest in guestlist)
+    foreach (List<string> bookings in stayslist) // iterate through bookinglist
     {
-        Membership membership = new Membership();
-        Stay stay = new Stay();
-        guestList.Add(new Guest(guest[0], guest[1], stay, membership));
+        Stay book = new Stay(Convert.ToDateTime(bookings[3]), Convert.ToDateTime(bookings[4]));
+        foreach (List<string> rooms in roomlist) // iterate through roomslist
+        {
+            if (rooms[1] == bookings[5]) // if roomno matches
+            {
+                if (rooms[0] == "Standard") // if type is standard
+                {
+                    StandardRoom room = new StandardRoom(Convert.ToInt32(rooms[1]), rooms[2], Convert.ToDouble(rooms[3]), !(Convert.ToBoolean(bookings[2])));
+                    room.RequireWifi = Convert.ToBoolean(bookings[6]);
+                    room.RequireBreakfast = Convert.ToBoolean(bookings[7]);
+                    book.AddRoom(room);
+                    
+                }
+                else if (rooms[0] == "Deluxe") // if type is deluxe
+                {
+                    DeluxeRoom room = new DeluxeRoom(Convert.ToInt32(rooms[1]), rooms[2], Convert.ToDouble(rooms[3]), !(Convert.ToBoolean(bookings[2])));
+                    room.AdditionalBed = Convert.ToBoolean(bookings[12]);
+                    book.AddRoom(room);
+                }
+                foreach (List<string> guest in guestlist)
+                {
+                    if (guest[1] == bookings[1])
+                    {
+                        Membership m = new Membership(guest[2], Convert.ToInt32(guest[3]));
+                        Guest guestobj = new Guest(guest[0], guest[1], book, m);
+                        guestobj.IsCheckedin = Convert.ToBoolean(bookings[2]);
+                        guestList.Add(guestobj);
+                    }
+                }
+            }
+        }
     }
 }
 
