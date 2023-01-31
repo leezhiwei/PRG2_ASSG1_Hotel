56ab4e242779bd54166f2b454259b5d04b5aa675
Howl-Wyhcz <limjiaxian2@gmail.com>
2023-01-31 12:51:17 +0800

    temp RoomService

---
+++ PRG2_ASSG1_Hotel/Food.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_ASSG1_Hotel
+{
+    internal class Food
+    {
+        public string FoodName { get; set; }
+        public double FoodPrice { get; set; }
+
+        public Food() { }
+        public Food(string fn, double fp)
+        {
+            FoodName = fn;
+            FoodPrice = fp;
+        }
+
+        public override string ToString()
+        {
+            return $"Food selected: {FoodName} Price: {FoodPrice}";
+        }
+    }
+}
---
+++ PRG2_ASSG1_Hotel/RoomService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_ASSG1_Hotel
+{
+    internal class RoomService
+    {
+        public List<Food> foodlist = new List<Food>();
+
+        public RoomService() { }
+        public double CalculateFoodCharges()
+        {
+            return 0;
+        }
+    }
+}
